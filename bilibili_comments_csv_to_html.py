5d95dab17498d8d7c337ee030f9fc94f5ea431f5
v <v>
2024-09-16 18:09:03 +0800

    init

---
+++ bilibili_comments_csv_to_html.py
@@ -0,0 +1,84 @@
+"""
+bilibili评论转为转为单页html
+依赖: https://github.com/linyuye/Bilibili_crawler 生成的test_3.csv
+"""
+
+import csv
+import operator
+
+from comments_to_html import Comment, RootComment, root_comments_to_html
+
+
+class CsvRow:
+    def __init__(self, column1, column2, column3, column4, column5, column6, column7, column8, column9, column10):
+        comment = Comment(
+            name = column1,
+            sex = column2,
+            time = column3.replace(' 北京时间', ''),
+            like = 0 if not column4 else int(column4),
+            content = column5,
+            location = column6,
+            uid = column9
+        )
+        self.comment = comment
+        self.sub_cnt = 0 if not column7 else int(column7)
+
+            
+def parse_csv_to_comments(filepath):
+    with open(filepath, mode='r', encoding='utf-8') as file:
+        reader = csv.reader(file)
+        next(reader)
+        return [CsvRow(*row) for row in reader]
+
+
+def comments_to_root_comments(comments):
+    root_comments = []
+    
+    idx = 0
+    cnt = 0
+    while True:
+        row = comments[idx]
+        comment = row.comment
+        cnt = cnt + 1
+        
+        sub_cnt = row.sub_cnt
+        if sub_cnt > 0:
+            child_comments = comments[(idx+1):(idx+1+sub_cnt)]
+            for obj in child_comments:
+                assert(obj.sub_cnt == sub_cnt), f'数据sub_cnt不匹配: idx: {idx}, sub_cnt: {sub_cnt}'
+            
+            idx = idx + 1 + sub_cnt
+            child_comments = [i.comment for i in child_comments]
+            child_comments = sorted(child_comments, key=operator.attrgetter('time'))
+        else:
+            child_comments = None
+            idx = idx + 1
+            
+        score = comment.like + sum_list(child_comments, 'like', 0)
+        
+        root_comment = RootComment(cnt, comment, child_comments, score)
+        root_comments.append(root_comment)
+        
+        if idx >= len(comments):
+            break
+    
+    return sorted(root_comments, key=operator.attrgetter('score'), reverse=True)
+
+        
+def sum_list(objs, prop, default=0):
+    if not objs:
+        return default
+    return sum(getattr(obj, prop) for obj in objs)
+    
+    
+def main():
+    f_path = 'test1_3.csv'
+    out_path = 'out.html'
+    
+    comments = parse_csv_to_comments(f_path)
+    root_comments = comments_to_root_comments(comments)
+    root_comments_to_html(root_comments, out_path)
+
+if __name__ == "__main__":
+    main()
+
---
+++ comments_to_html.py
@@ -0,0 +1,243 @@
+"""
+将评论列表转为单页html
+依赖: Jinja2
+"""
+from jinja2 import Template
+
+
+class Comment:
+    def __init__(self, name, sex=None, time=None, like=0, content=None, location=None, uid=None):
+        self.name = name
+        self.sex = sex
+        self.time = time
+        self.like = like
+        self.content = content
+        self.location = location
+        self.uid = uid
+
+class RootComment:
+    def __init__(self, id, comment, child_comments, score):
+        self.id = id
+        self.comment = comment
+        self.child_comments = child_comments
+        self.score = score
+
+    
+def root_comments_to_html(root_comments, out_path):
+    template = Template(html_template)
+    rendered_html = template.render(comments=root_comments)
+    with open(out_path, 'w') as f:
+        f.write(rendered_html)
+
+html_template = """
+<!DOCTYPE html>
+<html>
+<head>
+  <title>comments</title>
+  <style>
+    body {
+      font-family: sans-serif;
+    }
+    .user {
+      color: #61666d;
+    }
+    .root-comment-container {
+      margin-bottom: 36px;
+    }
+    .root-comment-content {
+      margin-top: 5px;
+    }
+    .comment-meta {
+      margin-top: 6px;
+      font-size: smaller;
+      color: #9499a0;
+    }
+    .child-comments-container {
+    }
+    .child-comment {
+      margin-left: 20px;
+      padding: 16px 0px 0px 34px;
+    }
+    .show-more-content {
+      cursor: pointer;
+      margin-left: 20px;
+      padding: 8px 0px 0px 34px;
+      font-size: smaller;
+      color: #9499a0;
+    }
+  </style>
+</head>
+<body>
+
+  <div id="root-comments"> <!-- 1 -->
+    {% for root_comment in comments %}
+      {% set comment = root_comment.comment %}
+      <div class="root-comment-container"> <!-- 2 -->
+        <div class="root-comment-content"><span class="user">{{ comment.name }}:</span> <span>{{ comment.content }}</span></div>
+        <div class="comment-meta">
+            {% if comment.time != None -%}
+                {{ comment.time }}  
+            {% endif -%}
+            {% if comment.location != None -%}
+               IP属地: {{ comment.location }}  
+            {% endif -%}
+            {% if comment.like != None -%}
+               👍 {{ comment.like }}  
+            {% endif -%}
+            {% if comment.sex != None -%}
+               性别: {{ comment.sex }}  
+            {% endif -%}
+            {% if comment.uid != None -%}
+               ID: {{ comment.uid }}  
+            {% endif -%}
+        </div>
+        
+         {% if root_comment.child_comments != None -%}
+            <div class="child-comments-container"> <!-- 3 -->
+              
+              <div class="child-comments"> <!-- 4 -->
+                  {% for child_comment in root_comment.child_comments %}
+                        <div class="child-comment">
+                                <span class="user">{{ child_comment.name }}:</span> <span>{{ child_comment.content }}</span>
+                                <div class="comment-meta">
+                                    {% if child_comment.time != None -%}
+                                        {{ child_comment.time }}  
+                                    {% endif -%}
+                                    {% if child_comment.location != None -%}
+                                       IP属地: {{ child_comment.location }}  
+                                    {% endif -%}
+                                    {% if child_comment.like != None -%}
+                                       👍 {{ child_comment.like }}  
+                                    {% endif -%}
+                                    {% if child_comment.sex != None -%}
+                                       性别: {{ child_comment.sex }}  
+                                    {% endif -%}
+                                    {% if child_comment.uid != None -%}
+                                       ID: {{ child_comment.uid }}  
+                                    {% endif -%}
+                                </div>
+                          </div>
+                    {% endfor %}
+                </div>  <!-- 4 -->
+                 
+                 
+                {% set child_comment_cnt = root_comment.child_comments | count %}
+                {% if child_comment_cnt > 3 -%}
+                  <div class="show-more-content" data-id="{{ root_comment.id }}">
+                        <span>共 {{ child_comment_cnt }}  条回复</span>
+                        <span class="show-more">查看更多</span>
+                        <span class="show-more-recovery">收起</span>
+                  </div>
+                {% endif -%}
+                
+            </div> <!-- 3 -->
+        {% endif -%}
+      </div> <!-- 2 -->
+    {% endfor %}
+  </div> <!-- 1 -->
+
+  <script>
+        function scrollToPrevElement(el) {
+            el.scrollIntoView({
+                behavior: 'smooth', // 平滑滚动
+                block: 'nearest',   // 尽量滚动到最近的可视区域
+                inline: 'nearest'
+            });
+        }
+        
+        function show_more(el) {
+            var parent = el.parentNode;  
+            var recovery_btn = Array.from(parent.children).find(sibling => sibling.classList.contains('show-more-recovery'));
+            
+            var siblings = Array.from(parent.parentNode.children);  
+            var siblingWithClassB = siblings.find(sibling => sibling.classList.contains('child-comments'));
+            if (siblingWithClassB) {
+                var children = siblingWithClassB.children
+                if (children && children.length) {
+                    var cnt = 0;
+                    var i = 0;
+                    for (; i < children.length; i++) {
+                        child = children[i];
+                        if (child.style.display == 'none') {
+                            child.style.display = '';
+                            cnt = cnt + 1;
+                            if (cnt >= 10) {
+                                break;
+                            }
+                        }
+                    }
+                    if (recovery_btn) {
+                        recovery_btn.style.display = '';
+                    }
+                    if (i >= children.length - 1) {
+                        el.style.display = 'none'
+                    }
+                }
+            }
+        }
+        function show_more_recovery(el) {
+            var parent = el.parentNode;  
+            var show_more_btn = Array.from(parent.children).find(sibling => sibling.classList.contains('show-more'));
+            var siblings = Array.from(parent.parentNode.children);  
+            var siblingWithClassB = siblings.find(sibling => sibling.classList.contains('child-comments'));
+            
+            if (siblingWithClassB) {
+                var children = siblingWithClassB.children
+                if (children && children.length) {
+                    for (var i = 3; i < children.length; i++) {
+                        child = children[i];
+                        if (child.style.display != 'none') {
+                            child.style.display = 'none';
+                        } else {
+                            break;
+                        }
+                    }
+                    
+                    el.style.display = 'none';
+                    show_more_btn.style.display = '';
+                    scrollToPrevElement(parent.parentNode.parentNode)
+                }
+            }
+        }
+        
+        document.addEventListener('DOMContentLoaded', function() {
+          var ele_as = document.querySelectorAll('.child-comments-container');
+          ele_as.forEach(function(ele_a) {
+              var ele_bs = ele_a.querySelectorAll('.child-comment');
+              if (ele_bs.length > 3) {
+                  for (let i = 3; i < ele_bs.length; i++) {
+                      ele_bs[i].style.display = 'none';
+                  }
+                  ele_a.querySelector('.show-more-recovery').style.display = 'none';
+              }
+          });
+      });
+      
+      document.addEventListener('click', function(event) {
+        var el = event.target;
+        if (el.classList.contains('show-more')) {
+          show_more(el);
+          return;
+        }
+        if (el.classList.contains('show-more-recovery')) {
+          show_more_recovery(el);
+          return;
+        }
+      }, true)
+  </script>
+</body>
+</html>
+"""
+
+    
+def main():
+    json_path = '/home/v/Downloads/test.json'
+    out_path = 'out.html'
+    
+    import json
+    root_comments = json.load(json_path)
+    root_comments_to_html(root_comments, out_path)
+
+if __name__ == "__main__":
+    main()
+
